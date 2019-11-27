222867df40081897f52288b4090797d37b0686ac
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2019-11-27 10:05:29 -0300

    careega Anuncios

--- app/src/main/java/com/example/olxapp/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/example/olxapp/activity/CadastrarAnuncioActivity.java
@@ -1,17 +1,166 @@
 
 package com.example.olxapp.activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
+import com.blackcat.currencyedittext.CurrencyEditText;
 import com.example.olxapp.R;
+import com.example.olxapp.helper.Permissoes;
+import com.santalu.maskedittext.MaskEditText;
 
-public class CadastrarAnuncioActivity extends AppCompatActivity {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class CadastrarAnuncioActivity extends AppCompatActivity implements  View.OnClickListener{
+
+    private EditText campoTitulo, campoDescricao;
+    private ImageView imagem1, imagem2, imagem3;
+    private Spinner campoEstado, campoCategoria;
+    private CurrencyEditText campoValor;
+    private MaskEditText campoTelefone;
+
+    private String[] permissoes = new String[]{
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+};
+    private List<String> listaFotosRecuperadas = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_anuncio);
+
+        //Validar permissoes
+        Permissoes.validarPermissoes(permissoes, this, 1);
+
+
+        inicializarComponentes();
+        carregarDadosSpinner();
+    }
+    public void salvarAnuncio(View view){
+
+        String valor = campoTelefone.getText().toString();
+        Log.d("Salvar", "Salvar anuncio: " + valor);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.imageCadastro1:
+                escolherImagem(1);
+                break;
+            case R.id.imageCadastro2:
+                escolherImagem(2);
+                break;
+            case R.id.imageCadastro3:
+                escolherImagem(3);
+                break;
+        }
+
+
+    }
+
+    public void escolherImagem(int requestCode){
+        Intent i  = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, requestCode);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if( resultCode == Activity.RESULT_OK){
+
+            //Recuperar imagem
+            Uri imagemSelecionada = data.getData();
+            String caminhoImagem = imagemSelecionada.toString();
+
+            //configura  imagem no ImageView
+            if( requestCode == 1){
+                imagem1.setImageURI(imagemSelecionada);
+            }else if (requestCode == 2){
+                imagem2.setImageURI(imagemSelecionada);
+            }else if(requestCode == 3){
+                imagem3.setImageURI(imagemSelecionada);
+            }
+            listaFotosRecuperadas.add(caminhoImagem);
+        }
+
+    }
+
+    private void carregarDadosSpinner(){
+
+        String[] estados = new String[]{
+
+        };
+
+    }
+
+    private void inicializarComponentes(){
+        campoTitulo = findViewById(R.id.editTitulo);
+        campoDescricao = findViewById(R.id.editDescricao);
+        campoValor = findViewById(R.id.editValor);
+        campoTelefone = findViewById(R.id.editTelefone);
+        campoEstado = findViewById(R.id.spinnerEstado);
+        campoCategoria = findViewById(R.id.spinnerCategoria);
+        imagem1 = findViewById(R.id.imageCadastro1);
+        imagem2 = findViewById(R.id.imageCadastro2);
+        imagem3 = findViewById(R.id.imageCadastro3);
+        imagem1.setOnClickListener(this);
+        imagem2.setOnClickListener(this);
+        imagem3.setOnClickListener(this);
+
+
+        Locale locale = new Locale("pt", "Br");
+        campoValor.setLocale(locale);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for(int permissaoResultado : grantResults){
+            if(permissaoResultado == PackageManager.PERMISSION_DENIED){
+                alertaValidacaoPermicao();
+            }
+        }
+
+    }
+    private void alertaValidacaoPermicao(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissoões Negadas");
+        builder.setMessage("Para utilizar o app é necessário aceitar as permissões");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
